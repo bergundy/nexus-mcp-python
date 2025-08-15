5e8d80f3f73763a092aefdebd03ec5ec523709af
Roey Berman <roey.berman@gmail.com>
2025-08-15 16:40:31 -0700

    Fix tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -27,8 +27,8 @@ async def test_list_tools() -> None:
         None,
     )
     assert len(tools) == 2
-    assert tools[0].name == "modified-service-name.modified-op-name"
-    assert tools[1].name == "modified-service-name.op2"
+    assert tools[0].name == "modified-service-name/modified-op-name"
+    assert tools[1].name == "modified-service-name/op2"
     assert tools[0].description == "This is a test operation."
     assert tools[1].description == "This is also a test operation."
     assert tools[0].inputSchema == MyInput.model_json_schema()
