ac63af2cb0676aee9c0c4808164ae2383fffff7e
Dan Davison <dandavison7@gmail.com>
2025-08-18 09:07:18 -0400

    Export WorkflowNexusTransport at top-level

--- nexusmcp/__init__.py
+++ nexusmcp/__init__.py
@@ -2,7 +2,8 @@ from temporalio import workflow
 
 with workflow.unsafe.imports_passed_through():
     from .inbound_gateway import InboundGateway
+    from .nexus_transport import WorkflowNexusTransport
     from .service import MCPService
     from .service_handler import MCPServiceHandler, exclude
 
-__all__ = ["MCPService", "MCPServiceHandler", "InboundGateway", "exclude"]
+__all__ = ["MCPService", "MCPServiceHandler", "InboundGateway", "exclude", "WorkflowNexusTransport"]
